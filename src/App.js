f8cd62b7366cf267a854df819f8ec67da714d0c5
Clement CHAMPOUILLON <clement@champouillon.com>
2018-03-29 21:31:02 +0200

    Removed package-lock.json, connected Redux

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header/Header'
-// import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer'
 
 import Login from './containers/Login/Login';
 import Signup from './containers/Signup/Signup';
@@ -12,13 +12,14 @@ import './App.css';
 
 const App = () => (
 		<BrowserRouter>
-			<div>
+			<div style={{ height: '100%', position: 'relative' }}>
 				<Header />
 				<Switch>
 					<Route path="/login" exact component={Login} />
 					<Route path="/signup" exact component={Signup} />
 					<Route path="/reset" exact component={Reset} />
 				</Switch>
+				<Footer />
 			</div>
 		</BrowserRouter>
 );
