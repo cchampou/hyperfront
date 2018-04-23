eb25ca8e4120eefc142f0ce92e5cfcdd36a94c68
Clement CHAMPOUILLON <cchampou@e2r6p12.42.fr>
2018-04-23 03:06:53 +0200

    Background de ouf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header/Header'
@@ -12,13 +12,63 @@ import Account from './containers/Account/Account';
 import ResetPass from './containers/ResetPass/ResetPass';
 import Auth from './containers/Auth/Auth';
 import Profile from './containers/Profile/Profile';
+import axios from 'axios';
+
+
 
 import './App.css';
 
-const App = () => (
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			background : '',
+			hide : true
+		}	
+	}
+
+	randomize = () => {
+		this.setState({
+			hide : true
+		});
+		axios.get('https://api.unsplash.com/photos/random?query=movie', {
+			headers : {
+				'Authorization' : 'Client-ID 4f8ab43cc45f8952da4850f074dfc89bc8e3bf6c4da619bea8d6c9b4a68544a3'
+			}
+		}).then((res) => {
+			this.setState({
+				background : res.data.urls.full
+			});
+			setTimeout(() => {
+				this.setState({
+					hide : false
+				})
+			}, 1000);
+		}).catch((err) => {
+			this.setState({
+				background : 'https://images.unsplash.com/photo-1521967906867-14ec9d64bee8?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjI1MTMwfQ&s=381aec77987962586079df5424386844'
+			});
+			setTimeout(() => {
+				this.setState({
+					hide : false
+				})
+			}, 1000);
+		})
+	}
+
+	componentDidMount() {
+		this.randomize();
+		setInterval(() => this.randomize(), 20000);
+	}
+
+	render () {
+		return (
 		<BrowserRouter>
-			<div style={{ minHeight: '100%', position: 'relative', paddingBottom: '60px' }}>
+			<div style={{ minHeight: '100%', position: 'relative', paddingBottom: '60px', backgroundPosition : 'center', backgroundSize : 'cover', backgroundImage : 'url('+this.state.background+')' }}>
 				<Header />
+				<div style={{ backgroundColor : 'rgba(0, 0, 0, 0.5)', position: 'fixed', height : '100%', width: '100%' }}></div>
+				<div style={{ backgroundColor : 'black', opacity: (this.state.hide?'1':'0'), position: 'fixed', height : '100%', width: '100%', transition: 'opacity 0.5s' }}></div>
 				<Switch>
 					<Route path="/" exact component={Search} />
 					<Route path="/play/:id" exact component={Play} />
@@ -32,6 +82,8 @@ const App = () => (
 				<Footer />
 			</div>
 		</BrowserRouter>
-);
+		)
+	}
+}
 
 export default App;
