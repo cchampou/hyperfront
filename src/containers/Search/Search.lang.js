fe8e52bf1bafa93c0c64f8193e42c29e71551466
Clement CHAMPOUILLON <cchampou@e2r7p15.42.fr>
2018-04-23 17:40:08 +0200

    Trad

--- src/containers/Search/Search.lang.js
+++ src/containers/Search/Search.lang.js
@@ -10,3 +10,39 @@ export const signup = lang => (
 export const slogan = lang => (
     (lang === 'fr')?"Toujours plus de films":"More and more movies"
 )
+
+export const seen = lang => (
+    (lang === 'fr')?"Vu":"Seen"
+)
+
+export const notseen = lang => (
+    (lang === 'fr')?"Non vu":"Not seen"
+)
+
+export const sort = lang => (
+    (lang === 'fr')?"Trier par":"Sort by"
+)
+
+export const filterdate = lang => (
+    (lang === 'fr')?"Filrer par date":"Filter by date"
+)
+
+export const filternote = lang => (
+    (lang === 'fr')?"Filrer par note":"Filter by vote"
+)
+
+export const pop = lang => (
+    (lang === 'fr')?"Popularité":"Popularity"
+)
+
+export const note = lang => (
+    (lang === 'fr')?"Note":"Vote"
+)
+
+export const date = lang => (
+    (lang === 'fr')?"Date de sortie":"Release date"
+)
+
+export const search = lang => (
+    (lang === 'fr')?"Rechercher...":"Search..."
+)
